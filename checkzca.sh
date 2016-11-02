7f4ae2beaa06c1b3868521b0522bb5a2cc01ea41
Will Norvelle <willnorvelle@gmail.com>
2016-11-02 18:56:15 -0400

    added ability to email results rather than just print them

--- checkzca.sh
+++ checkzca.sh
@@ -4,9 +4,17 @@
 #checkzca.sh
 
 unbalance=`curl -s 'http://zcash.flypool.org/api/miner_new/t1MEGqAZrURx8kpQi3KLSXT1fNkQqoxQsEB' | python -c "import sys, json; print(float(json.load(sys.stdin)['unpaid']) * (10**-8))"`
-echo "unpaid balance: $unbalance"
+
 zecusd=`curl -s 'https://api.coinmarketcap.com/v1/ticker/zcash/' | python -c "import sys, json; print(json.load(sys.stdin)[0]['price_usd'])"`
-echo "ZEC to USD: \$$zecusd"
+
 usd=`awk "BEGIN {print $unbalance * $zecusd}"`
 #usd=`echo '"$unbalance" * "$zecusd"' | bc -l`
-echo "unpaid balance in USD: \$$usd"
+
+
+if ["$1" == "-m"]; then
+	printf "unpaid balance: $unbalance\nZEC to USD: \$$zecusd\nunpaid balance in USD: \$$usd" | mailx -s "zcash update" willnorvelle@gmail.com
+else
+	echo "unpaid balance: $unbalance"
+	echo "ZEC to USD: \$$zecusd"
+	echo "unpaid balance in USD: \$$usd"
+fi
