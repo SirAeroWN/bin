5de646848470a3ee992889888202c924d0866287
Will Norvelle <willnorvelle@gmail.com>
2016-11-02 18:08:17 -0400

    now checks ZEC to USD and displays balance in USD

--- checkzca.sh
+++ checkzca.sh
@@ -3,4 +3,10 @@
 #11/02/16 
 #checkzca.sh
 
-curl -s 'http://zcash.flypool.org/api/miner_new/t1MEGqAZrURx8kpQi3KLSXT1fNkQqoxQsEB' | python -c "import sys, json; print(float(json.load(sys.stdin)['unpaid']) * (10**-8))"
+unbalance=`curl -s 'http://zcash.flypool.org/api/miner_new/t1MEGqAZrURx8kpQi3KLSXT1fNkQqoxQsEB' | python -c "import sys, json; print(float(json.load(sys.stdin)['unpaid']) * (10**-8))"`
+echo "unpaid balance: $unbalance"
+zecusd=`curl -s 'https://api.coinmarketcap.com/v1/ticker/zcash/' | python -c "import sys, json; print(json.load(sys.stdin)[0]['price_usd'])"`
+echo "ZEC to USD: \$$zecusd"
+usd=`awk "BEGIN {print $unbalance * $zecusd}"`
+#usd=`echo '"$unbalance" * "$zecusd"' | bc -l`
+echo "unpaid balance in USD: \$$usd"
